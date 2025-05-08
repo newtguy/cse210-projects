11873b9a5f27763669b69a94e53d5c4989d97bd8
newt <nate.e.shelley@gmail.com>
2025-05-08 15:13:37 -0600

    week01-exercise1

--- week01/Exercise1/Program.cs
+++ week01/Exercise1/Program.cs
@@ -4,6 +4,16 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise1 Project.");
+        // Console.WriteLine("Hello World! This is the Exercise1 Project.");
+        Console.WriteLine("What is your first name?");
+        string userFirstName = Console.ReadLine();
+
+        Console.WriteLine("What is your last name?");
+        string userLastName = Console.ReadLine();
+
+        string combinedName = userFirstName + " " + userLastName;
+
+        Console.WriteLine($"Your name is {userLastName}, {combinedName}.");
     }
 }
+
